ff35eb32a46ef2f4a711c1afef1994d4b7f68272
Julia Melnikova <melnikova.iuv@phystech.edu>
2024-02-19 21:22:21 +0300

    Add all ctor; dtor; move-ctor

--- include/vector.hpp
+++ include/vector.hpp
@@ -13,37 +13,67 @@
 #include <type_traits>
 
 namespace containers {
-#if defined(__x86_64__)
-static unsigned BOOST_ARCH_WORD_BITS = 64;
-#elif defined(__i386__)
-static unsigned BOOST_ARCH_WORD_BITS = 32;
-#else
-static unsigned BOOST_ARCH_WORD_BITS = 32;
-#endif
+static constexpr unsigned BOOST_ARCH_WORD_BITS = 32;
 
-template <typename T> class vector {
-private:
-  T *buf_begin_ptr = nullptr;
-  T *buf_end_ptr = nullptr;
-  T *buf_capacity_ptr = nullptr;
+template <typename T>
+class vector {
+ private:
+  T* buf_begin_ptr = nullptr;
+  T* buf_end_ptr = nullptr;
+  T* buf_capacity_ptr = nullptr;
 
   static constexpr std::size_t default_capacity = 8;
 
-public:
+ public:
   vector()
-      : buf_begin_ptr{static_cast<T *>(
+      : buf_begin_ptr{static_cast<T*>(
             ::operator new(sizeof(T) * default_capacity))},
         buf_capacity_ptr{buf_begin_ptr + default_capacity},
         buf_end_ptr{buf_begin_ptr} {};
 
-  // vector(std::size_t count, T val = T{}) : {};
+  explicit vector(std::size_t count, T val = T{}) {
+    vector<T> tmp{};
+    tmp.expand(count);
+    std::ranges::copy(std::views::repeat_n(value, count),
+                      std::back_inserter(tmp));
+
+    *this = std::move(tmp);
+  };
+
+  vector(T* frst, T* lst) {
+    vector<T> tmp{};
+    std::copy(frst, lst, std::back_inserter(tmp));
+
+    *this = std::move(tmp);
+  };
+
+  ~vector() {
+    std::destroy(buf_begin_ptr, buf_end_ptr);
+    buf_end_ptr = buf_begin_ptr;
+    ::operator delete(buf_begin_ptr);
+  };
+
+  vector(vector&& rhs) noexcept {
+    std::swap(buf_begin_ptr, rhs.buf_begin_ptr);
+    std::swap(buf_end_ptr, rhs.buf_end_ptr);
+    std::swap(buf_capacity_ptr, rhs.buf_capacity_ptr);
+  }
+
+  vector(vector& rhs) {
+    vector tmp{};
+    tmp.expand(rhs.capacity());
+
+    *this = std::move(tmp);
+  }
 
   void expand(std::size_t cap) {
     if (cap <= capacity())
       return;
 
-    T *tmp_buf = static_cast<T *>(::operator new(sizeof(T) * cap));
-    auto deleter = [](T *ptr) { ::operator delete(ptr); };
+    T* tmp_buf = static_cast<T*>(::operator new(sizeof(T) * cap));
+    auto deleter = [](T* ptr) {
+      ::operator delete(ptr);
+    };
     std::unique_ptr<T, decltype(deleter)> raii_buf(tmp_buf, deleter);
 
     const std::size_t sz = size();
@@ -55,14 +85,14 @@ public:
     buf_capacity_ptr = buf_begin_ptr + cap;
   }
 
-private:
+ private:
   void expand_if_neccessary() {
     if (buf_capacity_ptr - buf_end_ptr > 0)
       return;
     expand(amortized(capacity()));
   }
 
-public:
+ public:
   std::size_t amortized(std::size_t sz) const {
     return std::size_t{1} << (BOOST_ARCH_WORD_BITS - __builtin_clz(sz));
   }
@@ -74,4 +104,4 @@ public:
 
   bool empty() const noexcept { return (size() == 0); }
 };
-} // namespace containers
+}  // namespace containers
